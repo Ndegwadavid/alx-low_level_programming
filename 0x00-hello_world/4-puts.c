89c96a9cf944b3700d5c927c4b01f57a66fa3585
Ndegwadavid <davidndegwa6869@gmail.com>
2022-09-08 09:30:37 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #!/bin/bash
 /*inputting file using the put functions*/
+
+
 #include<stdio.h>
+
+
 /*return always to be zero*/
 /*main - entry point*/
 
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle""\");
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
