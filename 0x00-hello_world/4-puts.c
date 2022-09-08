f01be85ce05b21ce0b006eefe3a2793079bb932c
Ndegwadavid <davidndegwa6869@gmail.com>
2022-09-08 09:25:45 -0700

    printing using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+/*inputting file using the put functions*/
+#include<stdio.h>
+/*return always to be zero*/
+/*main - entry point*/
+
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle""\");
+		return(0);
+}
+
