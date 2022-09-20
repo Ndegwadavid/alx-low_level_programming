56c1dce0504ce990f33c42924e3c9991ada030d7
Ndegwadavid <davidndegwa6869@gmail.com>
2022-09-20 00:15:35 -0700

    reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - the reverse gear
+ * @s: print this string
+ *
+ * Return: always 0.
+ */
+
+void print_rev(char *s);
+{
+	int i;
+
+	for (i = _strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
