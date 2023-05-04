48b63e1ba10dd8ffcb20adbf3c15b91a3f561990
David <davidndegwa6869@mail.com>
2023-05-04 06:18:13 -0400

    gotcha

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "2-get_bit.c"
+/**
+ * set_bit - sets a bit
+ * @n: integer to grab
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+	(*n) |= 1 << index;
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
+}
