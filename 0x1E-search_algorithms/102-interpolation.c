4ce29c6505b485ac8dc30c6756833ccc5ab919d5
david Njoroge <davidndegwa6869@gmail.com>
2023-12-12 15:07:56 +0300

    interpolation c code

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,46 +1,36 @@
 #include "search_algos.h"
 
 /**
- * print_i - helper func to print idx each check
- * @array: array
- * @i: index checked
- */
-void print_i(int *array, size_t i)
-{
-	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-}
-
-/**
- * interpolation_search - like binary search, but use idx based on formula
- * @array: given array of ints
- * @size: size of array
+ * interpolation_search - performs jump search
+ * @array: the integer array
+ * @size: its size
  * @value: value to search for
- * Return: index at which value's found
+ *
+ * Return: the index found or -1
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t l = 0;
-	size_t r = size - 1;
-	size_t pos;
+	size_t pos = 0, low = 0, high = size - 1;
 
-	if (array == NULL)
+	if (!array || !size)
 		return (-1);
 
-	pos = l + (((double)(r - l) / (array[r] - array[l]))
-		   * (value - array[l]));
-	while (pos < size)
+	while (1)
 	{
-		print_i(array, pos);
+		pos = low + (((double)(high - low) /
+			  (array[high] - array[low])) * (value - array[low]));
+		if (pos >= size)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			break;
+		}
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
-		else if (array[pos] < value)
-			l = pos + 1;
+		else if (array[pos] > value)
+			high = pos - 1;
 		else
-			r = pos - 1;
-		pos = l + (((double)(r - l) / (array[r] - array[l]))
-			   * (value - array[l]));
+			low = pos + 1;
 	}
-	printf("Value checked array[%lu] is out of range\n", pos);
 	return (-1);
 }
-
